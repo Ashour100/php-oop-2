5ab35dc3cfd447cf5fc719551bb0ec2158af5242
Ashour100 <interinstitute5@gmail.com>
2022-05-09 23:33:42 +0200

    Some functions added

--- data/Customer.php
+++ data/Customer.php
@@ -1,16 +1,34 @@
 <?php
-
+include_once __DIR__ . '/CreditCard.php';
 
 class Customer{
     public $name;
     public $lastName;
     public $age;
+    public $creditCard;
+    public $isRegistered;
+    public $discount;
 
-    public function __construct($name, $lastName, $age)
-    {
+    public function __construct($name, $lastName, $age, $creditCard, $isRegistered){
         $this->name = $name;
         $this->last_name = $lastName;
         $this->age = $age;
+        $this->setCreditCard($creditCard);
+        $this->isRegistered = $isRegistered;
+        $this->discount = $this->setDiscount();
+    }
+
+    public function setDiscount(){
+        if ($this->isRegistered) 
+            $this->discount = 20;
+        else
+            $this->discount = 0;
+        return $this->discount;
+    }
+
+    public function setCreditCard($creditCard){
+        if (!$creditCard instanceof CreditCard) return false;
+        $this->creditCard = $creditCard;
     }
 }
 
